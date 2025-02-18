15ac1a176e138075d97db0eed449a19da674f119
ItsShuvi <itsshuvi@gmail.com>
2025-02-18 20:53:55 +0300

    added bash language to treesitter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
     config = function()
         local configs = require("nvim-treesitter.configs")
         configs.setup({
-            ensure_installed = { "lua", "vim", "javascript", "html", "python", "markdown", "markdown_inline" },
+            ensure_installed = { "bash", "lua", "vim", "javascript", "html", "python", "markdown", "markdown_inline" },
             highlight = { enable = true },
             indent = { enable = true },
         })
