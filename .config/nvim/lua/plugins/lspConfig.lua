df0e3949c39e66e2b4f5c82ba42712457f88ba79
ItsShuvi <itsshuvi@gmail.com>
2025-02-18 20:57:04 +0300

    added bashls to LSPconfig

--- .config/nvim/lua/plugins/lspConfig.lua
+++ .config/nvim/lua/plugins/lspConfig.lua
@@ -9,7 +9,7 @@ return {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "pyright" }
+                ensure_installed = { "lua_ls", "pyright", "bashls" }
             })
         end
     },
@@ -19,6 +19,7 @@ return {
             local lspconfig = require("lspconfig")
             lspconfig.lua_ls.setup{}
             lspconfig.pyright.setup{}
+            lspconfig.bashls.setup{}
 
             vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
             vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
